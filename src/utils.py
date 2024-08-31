641ce4ce25185ff32106be41ef33c21002ffabbf
NikitaSiulin <n.siulin@rufago.ru>
2024-08-31 23:09:58 +0700

    refactor: notify_admins func

--- src/utils.py
+++ src/utils.py
@@ -84,3 +84,9 @@ async def send_message(telegram_id: int, message: str) -> None:
     url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={telegram_id}&text={message}"
     async with aiohttp.ClientSession() as session, session.get(url) as resp:
         await resp.text()
+
+
+async def notify_admins(message: str) -> None:
+    """Send a message to all admins."""
+    for admin in ADMINS:
+        await send_message(admin, message)
