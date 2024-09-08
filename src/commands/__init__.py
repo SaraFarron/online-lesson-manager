502cb0181dc79813a68d688e1f8c99e227e6f1ea
NikitaSiulin <n.siulin@rufago.ru>
2024-09-09 00:54:19 +0700

    feat: reschedule lessone command

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,8 +1,7 @@
 from commands.add_lesson import router as add_lesson_router
 from commands.cancel import router as cancel_router
 from commands.help import router as help_router
-
-# from commands.reschedule_lesson import router as reschedule_lesson_router
+from commands.reschedule_lesson import router as reschedule_lesson_router
 from commands.start import router as start_router
 from commands.today_schedule import router as today_schedule_router
 from commands.week_schedule import router as week_schedule_router
@@ -14,7 +13,7 @@ all_routers = [
     cancel_router,
     today_schedule_router,
     week_schedule_router,
-    # reschedule_lesson_router,
+    reschedule_lesson_router,
 ]
 
 
--- src/commands/reschedule_lesson.py
+++ src/commands/reschedule_lesson.py
@@ -1,21 +1,20 @@
-from models import Reschedule, Weekend
 from __future__ import annotations
 
 from datetime import datetime
-from typing import Literal
 
 from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import CallbackQuery, Message
 from sqlalchemy.orm import Session
 
 from config import config
 from database import engine
 from help import Commands
-from logger import log_func, logger
-from models import RestrictedTime, ScheduledLesson, User
-from utils import inline_keyboard, this_week
+from logger import log_func
+from models import Reschedule, ScheduledLesson, User, Weekend
+from utils import MAX_HOUR, StudentSchedule, inline_keyboard, this_week
 
 COMMAND = "/reschedule"
 
@@ -27,6 +26,7 @@ class Messages:
     NO_LESSONS = "Нет предстоящих занятий"
     CONFRIM = "Вы можете назначить новое время, чтобы перенести урок"
     CHOOSE_DATE = "Введите дату в формате ДД-ММ-ГГГГ, нельзя выбрать %s"
+    WRONG_WEEKDAY = "Нельзя выбрать %s"
     CHOOSE_TIME = "Выберите время"
     LESSON_ADDED = "Урок добавлен"
     NOT_REGISTERED = "Вы не зарегистрированы. Пожалуйста воспользуйтесь командой /start"
@@ -39,8 +39,13 @@ class Callbacks:
     CHOOSE_TIME = "reschesule_lesson_choose_time:"
 
 
+class ChooseNewDateTime(StatesGroup):
+    choose_date = State()
+    choose_time = State()
+
+
 @router.message(Command(COMMAND))
-@router.message(F.text == Commands.RESCHEDULE)
+@router.message(F.text == Commands.RESCHEDULE.value)
 @log_func
 async def reschedule_lesson_handler(message: Message) -> None:
     """Handler receives messages with `/reschedule` command."""
@@ -48,11 +53,15 @@ async def reschedule_lesson_handler(message: Message) -> None:
     with Session(engine) as session:
         user = session.query(User).filter(User.telegram_id == message.from_user.id).first()
         if user:
-            lessons = session.query(ScheduledLesson).filter(
-                ScheduledLesson.user_id == user.id,
-                ScheduledLesson.weekday >= today.weekday(),
-                ScheduledLesson.start_time >= today.time(),
-            ).all()
+            lessons = (
+                session.query(ScheduledLesson)
+                .filter(
+                    ScheduledLesson.user_id == user.id,
+                    ScheduledLesson.weekday >= today.weekday(),
+                    ScheduledLesson.start_time >= today.time(),
+                )
+                .all()
+            )
             if lessons:
                 weekdays = {d.weekday(): d for d in this_week()}
                 buttons = [(weekdays[lesson.weekday], Callbacks.CHOOSE_SL + str(lesson.id)) for lesson in lessons]
@@ -72,10 +81,12 @@ async def reschesule_lesson_choose_sl_handler(callback: CallbackQuery, state: FS
         lesson: ScheduledLesson = session.query(ScheduledLesson).get(lesson_id)
         if lesson:
             await state.update_data(lesson=lesson_id, user_id=lesson.user_id)
-            keyboard = inline_keyboard([
-                ("Отменить урок", Callbacks.CONFIRM),
-                ("Перенести на новую дату", Callbacks.CHOOSE_DATE),
-            ]).as_markup()
+            keyboard = inline_keyboard(
+                [
+                    ("Отменить урок", Callbacks.CONFIRM),
+                    ("Перенести на новую дату", Callbacks.CHOOSE_DATE),
+                ],
+            ).as_markup()
             await callback.message.answer(Messages.CONFRIM, reply_markup=keyboard)
 
 
@@ -106,13 +117,47 @@ async def reschedule_lesson_choose_date(callback: CallbackQuery, state: FSMConte
         user = session.query(User).get(state_data["user_id"])
         weekends = session.query(Weekend).filter(Weekend.teacher_id == user.teacher_id).all()
         weekends_str = ", ".join([config.WEEKDAY_MAP_FULL[w.weekday] for w in weekends])
-    await state.set_state("reschedule_lesson_choose_date")
+    await state.set_state(ChooseNewDateTime.choose_date)
     await callback.message.answer(Messages.CHOOSE_DATE % weekends_str)
 
 
-# TODO finish up
+@router.message(ChooseNewDateTime.choose_date)
+@log_func
+async def reschedule_lesson_choose_time(message: Message, state: FSMContext) -> None:
+    """Handler receives messages with `reschedule_lesson_choose_time` state."""
+    date = datetime.strptime(message.text, "%d-%m-%Y")  # noqa: DTZ007
+    state_data = await state.get_data()
+    with Session(engine) as session:
+        user = session.query(User).get(state_data["user_id"])
+        teacher_weekends = session.query(Weekend).filter(Weekend.teacher_id == user.teacher_id).all()
+        if date.weekday() in [w.weekday for w in teacher_weekends]:
+            await message.answer(Messages.WRONG_WEEKDAY % config.WEEKDAY_MAP_FULL[date.weekday()])
+            return
+        await state.update_data(date=date)
+        await state.set_state(ChooseNewDateTime.choose_time)
+        schedule = StudentSchedule(user)
+        buttons = [
+            (t.strftime("%H:%M"), Callbacks.CHOOSE_TIME + t.strftime("%H.%M"))
+            for t in schedule.available_times_for_date(date)
+        ]
+        await message.answer(Messages.CHOOSE_TIME, reply_markup=inline_keyboard(buttons).as_markup())
 
-# date: Mapped[Optional[Date]] = mapped_column(Date, nullable=True, default=None)
-# start_time: Mapped[Optional[Time]] = mapped_column(Time, nullable=True, default=None)
-# end_time: Mapped[Optional[Time]] = mapped_column(Time, nullable=True, default=None)
 
+@router.callback_query(F.data.startswith(Callbacks.CHOOSE_TIME))
+@log_func
+async def reschedule_lesson_create_reschedule(callback: CallbackQuery, state: FSMContext) -> None:
+    """Handler receives messages with `reschedule_lesson_create_reschedule` state."""
+    state_data = await state.get_data()
+    time = datetime.strptime(callback.data.split(":")[1], "%H.%M").time()  # noqa: DTZ007
+    with Session(engine) as session:
+        reschedule = Reschedule(
+            user=session.query(User).get(state_data["user_id"]),
+            source_date=state_data["date"],
+            source_time=time,
+            date=state_data["date"],
+            start_time=time,
+            end_time=time.replace(hour=time.hour + 1) if time.hour < MAX_HOUR else time.replace(hour=0),
+        )
+        session.add(reschedule)
+        session.commit()
+    await callback.message.answer(Messages.LESSON_ADDED)
