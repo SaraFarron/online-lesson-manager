00931975e2e57a4e9495c8c6ef1c366ceb81fe6c
NikitaSiulin <n.siulin@rufago.ru>
2024-11-13 01:49:53 +0700

    feat: teacher collections all methods

--- src/repositories/restrictions.py
+++ src/repositories/restrictions.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from datetime import date, time
 
 from models import RestrictedTime, Teacher, User, Vacations, Weekend, WorkBreak
@@ -14,6 +16,18 @@ class WeekendRepo(Repository):
         )
         self.session.add(weekend)
 
+    def all(self, teacher: Teacher | None = None, weekday: int | None = None, start_time: time | None = None):
+        """Get all entries of model from the database."""
+        query = self.session.query(Weekend)
+        filters = {}
+        if teacher:
+            filters["teacher"] = teacher
+        if weekday is not None:
+            filters["weekday"] = weekday
+        if start_time:
+            filters["start_time"] = start_time
+        return query.filter_by(**filters).all()
+
 
 class VacationsRepo(Repository):
     def new(self, teacher: Teacher, start_date: date, end_date: date) -> None:
@@ -25,6 +39,18 @@ class VacationsRepo(Repository):
             end_date=end_date,
         )
         self.session.add(vacation)
+
+    def all(self, teacher: Teacher | None = None, start_date: date | None = None, end_date: date | None = None):
+        """Get all entries of model from the database."""
+        query = self.session.query(Vacations)
+        filters = {}
+        if teacher:
+            filters["teacher"] = teacher
+        if start_date:
+            filters["start_date"] = start_date
+        if end_date:
+            filters["end_date"] = end_date
+        return query.filter_by(**filters).all()
 
 
 class WorkBreakRepo(Repository):
@@ -38,6 +64,18 @@ class WorkBreakRepo(Repository):
             end_time=end_time,
         )
         self.session.add(work_break)
+
+    def all(self, teacher: Teacher | None = None, weekday: int | None = None, start_time: time | None = None):
+        """Get all entries of model from the database."""
+        query = self.session.query(WorkBreak)
+        filters = {}
+        if teacher:
+            filters["teacher"] = teacher
+        if weekday is not None:
+            filters["weekday"] = weekday
+        if start_time:
+            filters["start_time"] = start_time
+        return query.filter_by(**filters).all()
 
 
 class RestrictedTimeRepo(Repository):
@@ -54,5 +92,29 @@ class RestrictedTimeRepo(Repository):
         self.session.add(restricted_time)
 
 
-class RestrictionsRepo(Repository):
-    pass
+class TeacherRestTimeRepo(Repository):
+    def new(self, restrict_type: Weekend | Vacations | WorkBreak, *args, **kwargs):  # noqa: ANN002, ANN003
+        """Add new entry of model to the database."""
+        if type(restrict_type) is Weekend:
+            repo = WeekendRepo(self.type_model, self.session)
+        elif type(restrict_type) is Vacations:
+            repo = VacationsRepo(self.type_model, self.session)
+        elif type(restrict_type) is WorkBreak:
+            repo = WorkBreakRepo(self.type_model, self.session)
+        else:
+            msg = "Unknown restriction type"
+            raise TypeError(msg)
+        return repo.new(*args, **kwargs)
+
+    def all(
+        self,
+        teacher: Teacher | None = None,
+        weekday: int | None = None,
+        date: date | None = None,
+        time: time | None = None,
+    ):
+        """Get all entries of model from the database."""
+        work_breaks = WorkBreakRepo(self.type_model, self.session).all(teacher, weekday, time)
+        vacations = VacationsRepo(self.type_model, self.session).all(teacher, date, date)
+        weekends = WeekendRepo(self.type_model, self.session).all(teacher, weekday, time)
+        return work_breaks + vacations + weekends
