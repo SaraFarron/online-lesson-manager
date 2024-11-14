ce9eb8289d0d81d5eaa53b33bd236c6d3410b3b5
NikitaSiulin <n.siulin@rufago.ru>
2024-11-14 23:51:53 +0700

    fix: added get_by_telegram_id method

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from sqlalchemy.orm import Session
 
 from models import Teacher, User
@@ -19,13 +21,24 @@ class UserRepo(Repository):
         )
         self.session.add(user)
 
+    def get_by_telegram_id(self, telegram_id: int) -> User | None:
+        """Get user by telegram id."""
+        return self.session.query(User).filter(User.telegram_id == telegram_id).first()
+
 
 class TeacherRepo(Repository):
     def __init__(self, session: Session) -> None:
         """Initialize teacher repository class."""
         super().__init__(Teacher, session)
 
-    def new(self, full_name: str, telegram_id: int) -> None:
+    def new(self, full_name: str, telegram_id: int):
         """Add new entry of model to the database."""
         teacher = Teacher(name=full_name, telegram_id=telegram_id)
         self.session.add(teacher)
+        return teacher
+
+    def register(self, full_name: str, telegram_id: int, username: str):
+        """Creates teacher and user in db."""
+        teacher = self.new(full_name, telegram_id)
+        UserRepo(self.session).new(full_name, telegram_id, teacher, username)
+        return teacher
