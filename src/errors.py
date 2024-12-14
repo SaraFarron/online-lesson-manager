1f0da74ca82134539da9d0317c61e93f79c6ea8c
NikitaSiulin <n.siulin@rufago.ru>
2024-12-14 16:42:46 +0700

    feat: added errors

--- src/errors.py
+++ src/errors.py
@@ -3,10 +3,16 @@ from aiogram.filters.exception import ExceptionTypeFilter
 from aiogram.types.error_event import ErrorEvent
 from aiogram.types.message import Message
 
-import messages
+from config.config import ADMINS
+from messages import errors as err_msgs
+from utils import send_message
 
 
 class PermissionDeniedError(Exception):
+    pass
+
+
+class AiogramTelegramError(Exception):
     pass
 
 
@@ -15,10 +21,15 @@ def add_errors(dp: Dispatcher):
 
     @dp.errors(ExceptionTypeFilter(PermissionDeniedError), F.update.message.as_("message"))
     async def permission_denied(event: ErrorEvent, message: Message) -> None:
-        await message.answer(messages.PERMISSION_DENIED)
+        await message.answer(err_msgs.PERMISSION_DENIED)
 
-    @dp.errors(ExceptionTypeFilter(ValueError), F.update.message.as_("message"))
+    @dp.errors(ExceptionTypeFilter(AiogramTelegramError), F.update.message.as_("message"))
+    async def aiogram_telegram_error(event: ErrorEvent, message: Message) -> None:
+        await send_message(ADMINS["sara_farron"], f"error occured: {event.exception}\nmessage: {message}")
+        await message.answer(err_msgs.TELEGRAM_ERROR_OCCURED)
+
+    @dp.errors(ExceptionTypeFilter(Exception), F.update.message.as_("message"))
     async def value_error(event: ErrorEvent, message: Message) -> None:
-        await message.answer("error occured")
+        await message.answer(err_msgs.UNKNOWN)
 
     return dp
