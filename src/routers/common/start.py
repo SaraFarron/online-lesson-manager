9c70555e8b99699544dab4e59f56641c5b473baa
sara <ijcreepers@gmail.com>
2025-05-02 01:12:02 +0700

    feat: start command

--- src/routers/common/start.py
+++ src/routers/common/start.py
@@ -3,12 +3,10 @@ from aiogram.filters import CommandObject, CommandStart
 from aiogram.types import Message
 from sqlalchemy.orm import Session
 
-from config import config
-from errors import AiogramTelegramError, PermissionDeniedError
-from messages import replies
-from middlewares import DatabaseMiddleware
-from repositories import UserRepo
-from service import RegistrationService
+from src.errors import AiogramTelegramError
+from src.messages import replies
+from src.middlewares import DatabaseMiddleware
+from src.repositories import UserRepo
 
 router: Router = Router()
 router.message.middleware(DatabaseMiddleware())
@@ -21,22 +19,10 @@ async def start_handler(message: Message, command: CommandObject, db: Session) -
     if message.from_user is None:
         raise AiogramTelegramError
     tg_id, tg_full_name, tg_username = message.from_user.id, message.from_user.full_name, message.from_user.username
-    if tg_id in config.BANNED_USERS:
-        raise PermissionDeniedError
     user_repo = UserRepo(db)
     user = user_repo.get_by_telegram_id(tg_id)
     if user is None:
-        registration = RegistrationService(db)
-        for code, admin_id in config.ADMINS.items():
-            if command.args == code:
-                registration.register(admin_id, tg_id, tg_full_name, tg_username)
-                break
-        else:
-            raise PermissionDeniedError
-    else:
-        if not user.telegram_username:
-            user.telegram_username = tg_username
-        db.commit()
+        user_repo.register(tg_id, tg_full_name, tg_username, "STUDENT", command.args)
 
     await message.answer(replies.GREETINGS % html.bold(tg_full_name))
     await message.answer(replies.BOT_DESCRIPTION)
