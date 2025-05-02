8208f33c670eb2ca7ba5733e57bbf6933ad95a5f
sara <ijcreepers@gmail.com>
2025-05-03 00:44:13 +0700

    feat: help complete

--- src/routers/common/help.py
+++ src/routers/common/help.py
@@ -8,8 +8,7 @@ from sqlalchemy.orm import Session
 from src.keyboards import Keyboards
 from src.messages import replies
 from src.middlewares import DatabaseMiddleware
-from src.models import EventHistory
-from src.repositories import UserRepo
+from src.repositories import EventHistoryRepo, UserRepo
 from src.utils import telegram_checks
 
 COMMAND = "help"
@@ -22,7 +21,5 @@ async def help_handler(message: Message, db: Session) -> None:
     """Handler receives messages with `/help` command."""
     message = telegram_checks(message)
     user = UserRepo(db).get_by_telegram_id(message.from_user.id)
-    log = EventHistory(author=user.username, scene="help", event_type="help", event_value="")
-    db.add(log)
-    db.commit()
     await message.answer(replies.HELP_MESSAGE, reply_markup=Keyboards.all_commands(user.role))
+    EventHistoryRepo(db).create(user.username, "help", "help", "")
