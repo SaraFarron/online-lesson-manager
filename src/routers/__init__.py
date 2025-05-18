f5c10cc04c5dd1b3b1020688962db9586f7f98fa
sara <ijcreepers@gmail.com>
2025-05-18 16:14:13 +0700

    refactor: clean up

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -11,7 +11,8 @@ from src.routers.lessons.day_schedule import router as day_schedule_router
 from src.routers.lessons.week_schedule import router as week_schedule_router
 
 # Schedule
-from src.routers.schedule.work_hours import router as work_hours_router
+from src.routers.schedule.work_schedule import router as work_scheduel_router
+from src.routers.schedule.vacations import router as vacations_router
 
 all_routers = [
     cancel_router,
@@ -22,7 +23,8 @@ all_routers = [
     move_lesson_router,
     day_schedule_router,
     week_schedule_router,
-    work_hours_router,
+    work_scheduel_router,
+    vacations_router,
 ]
 
 __all__ = ["all_routers"]
--- src/routers/work_schedule/__init__.py
+++
@@ -1,4 +0,0 @@
-from routers.set_working_hours import breaks, handler, weekends, work_hours  # noqa: F401
-from routers.set_working_hours.config import router
-
-__all__ = ["router"]
