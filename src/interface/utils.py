dfb933fb92a3df5f90097957d425c4cc53736041
sara <ijcreepers@gmail.com>
2025-11-19 13:03:23 +0700

    fix: day type

--- src/interface/utils.py
+++ src/interface/utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date, datetime
 
 from sqlalchemy.orm import Session
 
@@ -7,8 +7,10 @@ from src.service.dispatcher.check_work_breaks import schedule_work_break
 from src.service.utils import send_message
 
 
-async def auto_place_work_breaks(db: Session, user: UserSchema, date: datetime, executor_tg: int) -> None:
-    work_breaks = schedule_work_break(db, user.executor_id, date.date())
+async def auto_place_work_breaks(db: Session, user: UserSchema, day: datetime | date, executor_tg: int) -> None:
+    if isinstance(day, datetime):
+        day = day.date()
+    work_breaks = schedule_work_break(db, user.executor_id, day)
     for brk in work_breaks:
         start_str = brk.start.strftime("%H:%M")
         await send_message(executor_tg, f"Автоматически добавлен перерыв на {start_str}")
